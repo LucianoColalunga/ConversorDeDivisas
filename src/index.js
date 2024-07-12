b8ce96d5fecc73ff8bc63b037503a52426835996
Luciano <129859436+LucianoColalunga@users.noreply.github.com>
2024-07-12 14:42:22 -0300

    Se trae informacion desde la API
    Se mostro por medio de lista la informacion de que se adquiere desde la API, donde con el evento click en el boton convertir se muestra la lista y se cambia el H1 para poder mostrar base de la monda(USD ejemplo) y su date(fehca ejemplo)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-var apikey='7915c2bad639d7babe825060'
-$.ajax({
-    method: "GET",       
-     url:"https://v6.exchangerate-api.com/v6/7915c2bad639d7babe825060/latest/USD"+ apikey + "/latest/USD",
+/// < reference types="jquery" />
+//key - "7915c2bad639d7babe825060";
 
-success: function (respuesta){
-    console.log("respuesta de: ", respuesta);
-},
-
-error: function(error){
-console.error("Error al realizar la solicutud", error);
-
-}
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#convertir").addEventListener('click', function () {
 
 
-});
+        fetch("https://v6.exchangerate-api.com/v6/7915c2bad639d7babe825060/latest/USD")
+            .then(respuesta => respuesta.json())
+            .then(respuestasJSON => {                
+                $('h1').text(`Cambios del dia ${respuestasJSON.time_last_update_utc} en base ${respuestasJSON.base_code}`);
+
+                $("#lista").empty();
+
+                Object.keys(respuestasJSON.conversion_rates).forEach(moneda => {
+                    $("#lista").append(`<li>${moneda}: ${respuestasJSON.conversion_rates[moneda]}</li>`);
+
+                });
+
+
+            })
+            .catch(error => {
+                console.error('Error al obtener los datos:', error)
+                $('h1').text('Error al cargar los datos');
+            })
+    })
+})
+
+    
+
+
+
+
+
+
+
+
+
+
